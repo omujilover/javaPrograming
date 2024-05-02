8d43224cd528e4b7b1139983411e9d8750f86177
admin <admin@DESKTOP-MGEK2QM>
2024-05-02 12:50:54 +0900

    week8 wee

---
+++ javaProgramming/src/week8/BreakExample.java
@@ -0,0 +1,23 @@
+package week8;
+
+import java.util.Scanner;
+
+public class BreakExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("exit을 입력하면 종료됩니다");
+		while (true) {
+			System.out.println(">>");
+			String text = scanner.nextLine();
+			if(text.equals("exit"))
+				break;
+		}
+		System.out.println("종료합니다...");
+		
+		scanner.close();
+	}
+
+}
