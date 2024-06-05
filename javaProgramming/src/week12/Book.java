a6535ca17a967f0822e2fbd4fac98cb15ded3b88
admin <admin@DESKTOP-MGEK2QM>
2024-06-05 11:54:00 +0900

    week12

---
+++ javaProgramming/src/week12/Book.java
@@ -0,0 +1,22 @@
+package week12;
+
+public class Book {
+	String title;
+	String author;
+	
+	public Book(String t) {
+		title = t; author = "작자미상";
+	}
+	public Book(String t, String a) {
+		title = t; author = a;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book littlePrince = new Book("어린왕자", "생텍쥐페리");
+		Book loveStory = new Book("춘항전");
+		System.out.println(littlePrince.title + " " + littlePrince.author);
+		System.out.println(loveStory.title + " " + loveStory.author);
+	}
+
+}
